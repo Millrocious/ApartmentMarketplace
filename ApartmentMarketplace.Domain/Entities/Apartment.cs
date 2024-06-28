0a8b8074d8b079451ce5ba00b7e4734e2980ab5f
Millrocious <vitalii.merdak.20@pnu.edu.ua>
2024-06-28 21:14:32 +0300

    Update Entity

--- ApartmentMarketplace.Domain/Entities/Apartment.cs
+++ ApartmentMarketplace.Domain/Entities/Apartment.cs
@@ -5,6 +5,6 @@ public class Apartment
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public int Rooms { get; set; }
     public required string Name { get; set; }
-    public int Price { get; set; }
+    public decimal Price { get; set; }
     public required string Description { get; set; }
 }
